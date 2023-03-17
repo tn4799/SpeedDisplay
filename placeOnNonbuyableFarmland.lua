0dd29aba275d5d38bf25c86d858e916bec09bb5a
tn47 <tn4799@gmail.com>
2023-03-17 16:44:42 +0100

    removed print

--- placeOnNonbuyableFarmland.lua
+++ placeOnNonbuyableFarmland.lua
@@ -1,9 +1,4 @@
 ConstructionBrushPlaceable.verifyAccess = Utils.overwrittenFunction(ConstructionBrushPlaceable.verifyAccess, function (self, superFunc, x, y, z)
-    local specName = "spec_FS22_SpeedDisplay.placeableSpeedDisplay"
-    local speedDisplaySpec = self.placeable[specName]
-    print("farmlandId: " .. tostring(g_farmlandManager:getFarmlandIdAtWorldPosition(x, z)))
-    print("NO_OWNER_FARM_ID: " .. FarmlandManager.NOT_BUYABLE_FARM_ID)
-
     if SpecializationUtil.hasSpecialization(PlaceableSpeedDisplay, self.placeable.specializations) then
         if not self:hasPlayerPermission() then
             return ConstructionBrush.ERROR.NO_PERMISSION
