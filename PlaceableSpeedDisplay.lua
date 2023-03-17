bc3a7d30e54af7d391850fee1be7163e1d32f08b
tn47 <tn4799@gmail.com>
2023-03-17 12:11:54 +0100

    script version of gtx

--- PlaceableSpeedDisplay.lua
+++ PlaceableSpeedDisplay.lua
@@ -1,5 +1,6 @@
 PlaceableSpeedDisplay = {
     modName = g_currentModName,
+    baseXMLPath = "speedDisplay",
     specName = "placeableSpeedDisplay",
     prerequisitesPresent = function (specializations)
         return true
@@ -14,35 +15,39 @@ function PlaceableSpeedDisplay.registerFunctions(placeableType)
 end
 
 function PlaceableSpeedDisplay.registerEventListeners(placeableType)
-    SpecializationUtil.registerEventListener(placeableType, "onLoad", PlaceableSpeedDisplay.onLoad)
-    SpecializationUtil.registerEventListener(placeableType, "onDelete", PlaceableSpeedDisplay.onDelete)
+    SpecializationUtil.registerEventListener(placeableType, "onLoad", PlaceableSpeedDisplay)
+    SpecializationUtil.registerEventListener(placeableType, "onDelete", PlaceableSpeedDisplay)
     SpecializationUtil.registerEventListener(placeableType, "onFinalizePlacement", PlaceableSpeedDisplay)
 end
 
 function PlaceableSpeedDisplay.registerXMLPaths(schema, basePath)
     schema:setXMLSpecializationType("SpeedDisplay")
-    basePath = basePath .. PlaceableSpeedDisplay.specName
+    local baseXMLPath = basePath .. "." .. PlaceableSpeedDisplay.baseXMLPath
+    local baseSavegamePath = basePath .. ".SpeedDisplay." .. PlaceableSpeedDisplay.specName
 
-    schema:register(XMLValueType.NODE_INDEX,    basePath .. "#triggerNode", "Trigger of speed display. When driving thogh the speed is measured.")
-    schema:register(XMLValueType.NODE_INDEX,    basePath .. "#triggerMarkers", "Show trigger marker during placement to show where the trigger is. When placing hide trigger markers")
-    schema:register(XMLValueType.INT,           basePath .. "#speedLimit", "The allowed speed limit of the vehicle. When Speed is higher than this the text will be in another color", 50)
-    schema:register(XMLValueType.NODE_INDEX,    basePath .. ".display(?)#node", "Display start node")
-	schema:register(XMLValueType.STRING,        basePath .. ".display(?)#font", "Display font name")
-	schema:register(XMLValueType.STRING,        basePath .. ".display(?)#alignment", "Display text alignment")
-	schema:register(XMLValueType.FLOAT,         basePath .. ".display(?)#size", "Display text size")
-	schema:register(XMLValueType.FLOAT,         basePath .. ".display(?)#scaleX", "Display text x scale")
-	schema:register(XMLValueType.FLOAT,         basePath .. ".display(?)#scaleY", "Display text y scale")
-	schema:register(XMLValueType.STRING,        basePath .. ".display(?)#mask", "Display text mask")
-	schema:register(XMLValueType.FLOAT,         basePath .. ".display(?)#emissiveScale", "Display emissive scale")
-	schema:register(XMLValueType.COLOR,         basePath .. ".display(?)#colorFine", "Display text color when driving below the speed limit")
-    schema:register(XMLValueType.COLOR,         basePath .. ".display(?)#colorTooFast", "Display text color when driving above the speed limit")
-	schema:register(XMLValueType.COLOR,         basePath .. ".display(?)#hiddenColor", "Display text hidden color")
+    schema:register(XMLValueType.NODE_INDEX,    baseXMLPath .. "#triggerNode", "Trigger of speed display. When driving thogh the speed is measured.")
+    schema:register(XMLValueType.NODE_INDEX,    baseXMLPath .. "#triggerMarkers", "Show trigger marker during placement to show where the trigger is. When placing hide trigger markers")
+    schema:register(XMLValueType.INT,           baseXMLPath .. "#speedLimit", "The allowed speed limit of the vehicle. When Speed is higher than this the text will be in another color", 50)
+    schema:register(XMLValueType.NODE_INDEX,    baseXMLPath .. ".display(?)#node", "Display start node")
+	schema:register(XMLValueType.STRING,        baseXMLPath .. ".display(?)#font", "Display font name")
+	schema:register(XMLValueType.STRING,        baseXMLPath .. ".display(?)#alignment", "Display text alignment")
+	schema:register(XMLValueType.FLOAT,         baseXMLPath .. ".display(?)#size", "Display text size")
+	schema:register(XMLValueType.FLOAT,         baseXMLPath .. ".display(?)#scaleX", "Display text x scale")
+	schema:register(XMLValueType.FLOAT,         baseXMLPath .. ".display(?)#scaleY", "Display text y scale")
+	schema:register(XMLValueType.STRING,        baseXMLPath .. ".display(?)#mask", "Display text mask")
+	schema:register(XMLValueType.FLOAT,         baseXMLPath .. ".display(?)#emissiveScale", "Display emissive scale")
+	schema:register(XMLValueType.COLOR,         baseXMLPath .. ".display(?)#colorFine", "Display text color when driving below the speed limit")
+    schema:register(XMLValueType.COLOR,         baseXMLPath .. ".display(?)#colorTooFast", "Display text color when driving above the speed limit")
+	schema:register(XMLValueType.COLOR,         baseXMLPath .. ".display(?)#hiddenColor", "Display text hidden color")
+
+    schema:register(XMLValueType.INT,           baseSavegamePath .. "#speedLimit", "The allowed speed limit of the vehicle. When Speed is higher than this the text will be in another color. Value in placeables.xml", 50)
+
 	schema:setXMLSpecializationType()
 end
 --load from xml file of the storeItem
 function PlaceableSpeedDisplay:onLoad(savegame)
     local spec = self[PlaceableSpeedDisplay.specPath]
-    local key = "placeable.speedDisplay"
+    local key = "placeable." .. PlaceableSpeedDisplay.baseXMLPath
 
     spec.trigger = self.xmlFile:getValue(key .. "#triggerNode", nil, self.components, self.i3dMappings)
 
@@ -64,7 +69,7 @@ function PlaceableSpeedDisplay:onLoad(savegame)
 
     spec.speedLimit = self.xmlFile:getValue(key .. "#speedLimit", 50)
 
-    spec.vehicle = {}
+    spec.vehicle = nil -- No need to create an empty table here just use nil
     spec.displays = {}
 
     --load displays
@@ -92,11 +97,19 @@ function PlaceableSpeedDisplay:onLoad(savegame)
 				}, true)
                 local colorTooFast = self.xmlFile:getValue(displayKey .. "#colorTooFast", colorFine, true)
 				local hiddenColor = self.xmlFile:getValue(displayKey .. "#hiddenColor", nil, true)
-				display.displayNode = displayNode
+
+				-- Using the same node will mean that the displays will flicker as changing visibility will affect both character lines.
+				-- Solution is to clone the displayLine node and update separately
+				-- NOTE: The parameters for 'clone' are 'objectId, groupUnderParent, callOnCreate, addPhysics' so by using 'groupUnderParent' the node is auto linked to the same location as displayNode
+				display.displayNodeFine = displayNode
+				display.displayNodeToFast = clone(displayNode, true, false, false)
+
 				display.formatStr, display.formatPrecision = string.maskToFormat(mask)
 				display.fontMaterial = fontMaterial
-				display.characterLineFine = fontMaterial:createCharacterLine(display.displayNode, mask:len(), size, colorFine, hiddenColor, emissiveScale, scaleX, scaleY, alignment)
-				display.characterLineTooFast = fontMaterial:createCharacterLine(display.displayNode, mask:len(), size, colorTooFast, hiddenColor, emissiveScale, scaleX, scaleY, alignment)
+
+				-- Apply the new node ids so each characterLine is separate
+				display.characterLineFine = fontMaterial:createCharacterLine(display.displayNodeFine, mask:len(), size, colorFine, hiddenColor, emissiveScale, scaleX, scaleY, alignment)
+				display.characterLineTooFast = fontMaterial:createCharacterLine(display.displayNodeToFast, mask:len(), size, colorTooFast, hiddenColor, emissiveScale, scaleX, scaleY, alignment)
 
 				table.insert(spec.displays, display)
 			end
@@ -110,14 +123,14 @@ end
 function PlaceableSpeedDisplay:saveToXMLFile(xmlFile, key, usedModNames)
     local spec = self[PlaceableSpeedDisplay.specPath]
 
-    xmlFile:setValue(key .. "#speedLimit", spec.speedLimit or 50)
+    xmlFile:setInt(key .. "#speedLimit", spec.speedLimit or 50)
 end
 
 -- load from placeable xml of savegame
 function PlaceableSpeedDisplay:loadFromXMLFile(xmlFile, key)
     local spec = self[PlaceableSpeedDisplay.specPath]
 
-    spec.speedLimit = xmlFile:getValue(key .. "#speedLimit", 50)
+    spec.speedLimit = xmlFile:getInt(key .. "#speedLimit", 50)
 end
 
 function PlaceableSpeedDisplay:onDelete()
@@ -141,42 +154,55 @@ end
 function PlaceableSpeedDisplay:setDisplayNumbers(speed)
     local spec = self[PlaceableSpeedDisplay.specPath]
 
-    if speed == 0 then
+    if speed <= 0 then
         for _, display in pairs(spec.displays) do
-            setVisibility(display.displayNode, false)
+            setVisibility(display.displayNodeFine, false)
+            setVisibility(display.displayNodeToFast, false)
         end
     else
         for _, display in pairs(spec.displays) do
-            setVisibility(display.displayNode, true)
-
             local int, floatPart = math.modf(speed)
             local value = string.format(display.formatStr, int, math.abs(math.floor(floatPart * 10^display.formatPrecision)))
 
-            local characterLine = display.characterLineFine
-            if speed > spec.speedLimit then
-                characterLine = display.characterLineTooFast
+			-- This will guarantee only one node is visible at a time
+			local isSpeeding = speed > spec.speedLimit
+
+			setVisibility(display.displayNodeFine, not isSpeeding)
+			setVisibility(display.displayNodeToFast, isSpeeding)
+
+			if isSpeeding then
+				display.fontMaterial:updateCharacterLine(display.characterLineTooFast, value)
+            else
+				display.fontMaterial:updateCharacterLine(display.characterLineFine, value)
             end
-		    display.fontMaterial:updateCharacterLine(characterLine, value)
         end
     end
 end
 
 function PlaceableSpeedDisplay:onSpeedDisplayTriggerCallback(triggerId, otherId, onEnter, onLeave, onStay)
-    local spec = self[PlaceableSpeedDisplay.specPath]
-    if onEnter then
-        local vehicle = g_currentMission:getNodeObject(otherId)
+	if onEnter or onLeave then
+		local spec = self[PlaceableSpeedDisplay.specPath]
+		local vehicle = g_currentMission:getNodeObject(otherId)
 
-        if vehicle ~= nil and vehicle.spec_driveable ~= nil and vehicle.spec_enterable and vehicle.spec_motorized then
-            spec.vehicle = vehicle
-        end
-    end
+		-- Best to make sure its is a drivable vehicle  for enter and leave or anything leaving will clear the display.
+		-- Also if the collision of the trigger uses BIT 20 then players would be detected and they do not have function 'getLastSpeed' so would cause error
+		if vehicle ~= nil and vehicle.spec_drivable ~= nil then
+			if onEnter then
+				-- By using 'vehicle ~= spec.vehicle' also it will mean that the vehicle speed is only checked when it first enters the trigger, otherwise as each vehicle component (otherId) enters the speed will change.
+				if vehicle ~= spec.vehicle then
+					spec.vehicle = vehicle
 
-    if onEnter or onStay then
-        local speed = MathUtil.round(spec.vehicle:getLastSpeed())
+					self:setDisplayNumbers(MathUtil.round(vehicle:getLastSpeed()))
+				end
+			-- Only clear the speed if it is the same vehicle leaving that triggered the speed to start with
+			elseif vehicle == spec.vehicle then
+				-- You should reset `spec.vehicle` to nil instead of using table constructors. when you use `{}` you are creating a new table each time and this is not a good practice in this situation.
+				-- Using this in onEnter 'if spec.vehicle ~= {} then' will also be false every time as you are creating a new table using the constructors each time ;-)
 
-        self:setDisplayNumbers(speed)
-    elseif onLeave then
-        spec.vehicle = {}
-        self:setDisplayNumbers(0)
-    end
+				spec.vehicle = nil -- Set to nil
+
+				self:setDisplayNumbers(0)
+			end
+		end
+	end
 end
